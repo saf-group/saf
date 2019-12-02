c76798cdca270c9de9f3e851e4ed037823aacf50
hepengyuan <hpy253215039@163.com>
2019-12-02 14:40:08 +0800

    开启DBPrometheusMonitorPlugin

--- saf-projects/saf-db/saf-db-druid/src/main/java/com/future/saf/db/druid/plugin/DBPrometheusMonitorPlugin.java
+++ saf-projects/saf-db/saf-db-druid/src/main/java/com/future/saf/db/druid/plugin/DBPrometheusMonitorPlugin.java
@@ -25,7 +25,7 @@ public class DBPrometheusMonitorPlugin implements Interceptor {
 			"mapper", "mapper", "mapper", new String[] { "resource", "method" },
 			new double[] { 0.0001, 0.0005, 0.0009, 0.001, 0.003, 0.005, 0.007, 0.009, 0.01, 0.03, 0.05, 0.07, 0.09, 0.1,
 					0.3, 0.5, 0.7, 0.9, 1, 3, 5 });
-
+	
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
 		if (!MonitorConfig.ENABLE_MONITOR_MAPPER) {

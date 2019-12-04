30b043cefac68671518669a7e20f6074331ecad2
hepengyuan <hpy253215039@163.com>
2019-12-04 17:27:56 +0800

    拆分saf, saf-boot-starter, saf-samples为3个工程

--- saf-samples/saf-sample-allinone/saf-sample-allinone-service/docker.build.sh
+++
@@ -1,2 +0,0 @@
-mvn clean package -Dmaven.test.skip=true
-docker build -t saf-sample-allinone-service:1.0.1-SNAPSHOT .
--- saf-samples/saf-sample-allinone/saf-sample-allinone-service/kubernetes/deploy.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p /datavip/k8s-data/saf-sample-allinone-service
-
-kubectl apply -f saf-sample-allinone-service-prod-namespace.yaml
-kubectl apply -f saf-sample-allinone-service-prod-pv-local.yaml
-kubectl apply -f saf-sample-allinone-service-prod-pvc.yaml
-kubectl apply -f saf-sample-allinone-service-prod-configmap.yaml
-kubectl apply -f saf-sample-allinone-service-prod-deployment.yaml
-kubectl apply -f saf-sample-allinone-service-prod-service.yaml

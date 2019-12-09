4b50e37ca963aed5e790f27c6558ac89f5ca0801
hepengyuan <hpy253215039@163.com>
2019-12-09 14:14:50 +0800

    摘出saf-samples

--- saf-sample/saf-sample-allinone/saf-sample-allinone-web/docker.build.sh
+++
@@ -1,2 +0,0 @@
-mvn clean package -Dmaven.test.skip=true
-docker build -t saf-sample-allinone-web:1.0.4-SNAPSHOT .
--- saf-sample/saf-sample-allinone/saf-sample-allinone-web/kubernetes/deploy.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p /datavip/k8s-data/saf-sample-allinone-web
-
-kubectl apply -f saf-sample-allinone-web-prod-namespace.yaml
-kubectl apply -f saf-sample-allinone-web-prod-pv-local.yaml
-kubectl apply -f saf-sample-allinone-web-prod-pvc.yaml
-kubectl apply -f saf-sample-allinone-web-prod-configmap.yaml
-kubectl apply -f saf-sample-allinone-web-prod-deployment.yaml
-kubectl apply -f saf-sample-allinone-web-prod-service.yaml
-kubectl apply -f saf-sample-allinone-web-prod-ingress.yaml

530e7bea9b8cf1dacf0984a4d457d3c78f965619
hpy <hpy253215039@163.com>
2020-01-24 20:46:42 +0800

    update

---
+++ saf-rpc/saf-rpc-dubbo/src/main/java/com/future/saf/rpc/dubbo/core/SafDubboAutoConfiguration.java
@@ -0,0 +1,24 @@
+package com.future.saf.rpc.dubbo.core;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+//import org.slf4j.Logger;
+//import com.future.saf.logging.basic.Loggers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.future.saf.core.autoconfiguration.CustomizedPropertiesBinderAutoConfiguration;
+
+@Configuration
+@AutoConfigureAfter(CustomizedPropertiesBinderAutoConfiguration.class)
+public class SafDubboAutoConfiguration {
+
+	// private static final Logger logger = Loggers.getFrameworkLogger();
+
+	@Bean
+	private ApplicationConfig applicationConfigBean() {
+		ApplicationConfig protocolConfig = new ApplicationConfig();
+		return protocolConfig;
+	}
+
+}
